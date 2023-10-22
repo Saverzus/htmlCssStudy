3ac1a302efd0a3796e035976996dde103424d51f
Saverzus <saverzus@gmail.com>
2023-10-22 19:13:24 +0300

    Уро 1 разметка готово
    Готово

--- 1 layout/swiperOurProjects.js
+++ 1 layout/swiperOurProjects.js
@@ -1,4 +1,10 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.our-projects__swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+var swiper = new Swiper('.certificates__swiper-container', {
     pagination: {
         el: '.swiper-pagination',
     },
