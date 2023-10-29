35b4f19f76ebb5925c9c5d2fab35fa7407d7e2f6
Saverzus <saverzus@gmail.com>
2023-10-29 19:43:35 +0300

    Картинки
    1

---
+++ 1 layout-imgs/swiperOurProjects.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.our-projects__swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+var swiper = new Swiper('.certificates__swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
